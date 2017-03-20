32933c8b08f0f4c85a2b6ab97cc8218f5e119094
Mient-jan Stelling <mientjan.stelling@gmail.com>
2017-03-20 16:59:02 -0300

    Changing bodymovin player to a es6 babel, webpack 2 build

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+let webpack = require('webpack');
+let path = require('path');
+
+let config = {
+	context: __dirname + '/player', // `__dirname` is root of project and `src` is source
+	entry: {
+		app: './js/index.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/, //Check for all js files
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['es2015']
+						}
+					}
+				]
+			},
+		]
+	},
+	output: {
+		path: __dirname + '/build', // `dist` is the destination
+		filename: 'bodymovin.js',
+	},
+	devServer: {
+		open: true, // to open the local server in browser
+		contentBase: __dirname + '/player',
+	},
+
+	devtool: "eval-source-map"
+};
+
+
+if (process.env.NODE_ENV === "production") {
+	config.devtool = "source-map";
+}
+
+module.exports = config;
